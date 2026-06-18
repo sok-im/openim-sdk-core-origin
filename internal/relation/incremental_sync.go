171cea7a4451389ce942c6a292f749f5a09b3e88
hawklin2017 <32898629+hawklin2017@users.noreply.github.com>
2026-06-19 00:32:00 +0800

    Revert "debug"
    This reverts commit 4f4cc491793070949e348fe3b7c1027c741dfe8a.

--- internal/relation/incremental_sync.go
+++ internal/relation/incremental_sync.go
@@ -6,12 +6,10 @@ import (
 	"github.com/openimsdk/openim-sdk-core/v3/pkg/db/model_struct"
 	"github.com/openimsdk/openim-sdk-core/v3/pkg/syncer"
 	"github.com/openimsdk/protocol/relation"
-	"github.com/openimsdk/tools/log"
 	"github.com/openimsdk/tools/utils/datautil"
 )
 
 func (r *Relation) IncrSyncFriends(ctx context.Context) error {
-	log.ZDebug(ctx, "lintao IncrSyncFriends", "loginUserID", r.loginUserID)
 	friendSyncer := syncer.VersionSynchronizer[*model_struct.LocalFriend, *relation.GetIncrementalFriendsResp]{
 		Ctx:       ctx,
 		DB:        r.db,
@@ -24,55 +22,11 @@ func (r *Relation) IncrSyncFriends(ctx context.Context) error {
 			return r.db.GetAllFriendList(ctx)
 		},
 		Server: func(version *model_struct.LocalVersionSync) (*relation.GetIncrementalFriendsResp, error) {
-			localVersion := uint64(0)
-			localVersionID := ""
-			if version != nil {
-				localVersion = version.Version
-				localVersionID = version.VersionID
-			}
-			log.ZInfo(ctx, "lintao IncrSyncFriends request",
-				"loginUserID", r.loginUserID,
-				"localVersion", localVersion,
-				"localVersionID", localVersionID)
-			resp, err := r.getIncrementalFriends(ctx, &relation.GetIncrementalFriendsReq{
+			return r.getIncrementalFriends(ctx, &relation.GetIncrementalFriendsReq{
 				UserID:    r.loginUserID,
-				Version:   localVersion,
-				VersionID: localVersionID,
+				Version:   version.Version,
+				VersionID: version.VersionID,
 			})
-			if err != nil {
-				log.ZError(ctx, "lintao IncrSyncFriends request failed", err,
-					"loginUserID", r.loginUserID)
-				return nil, err
-			}
-			updateUserIDs := make([]string, 0, len(resp.Update))
-			updateNicknames := make(map[string]string, len(resp.Update))
-			for _, item := range resp.Update {
-				if item.FriendUser == nil {
-					continue
-				}
-				updateUserIDs = append(updateUserIDs, item.FriendUser.UserID)
-				updateNicknames[item.FriendUser.UserID] = item.FriendUser.Nickname
-			}
-			insertUserIDs := make([]string, 0, len(resp.Insert))
-			for _, item := range resp.Insert {
-				if item.FriendUser == nil {
-					continue
-				}
-				insertUserIDs = append(insertUserIDs, item.FriendUser.UserID)
-			}
-			log.ZInfo(ctx, "lintao IncrSyncFriends response",
-				"loginUserID", r.loginUserID,
-				"serverVersion", resp.Version,
-				"serverVersionID", resp.VersionID,
-				"full", resp.Full,
-				"deleteCount", len(resp.Delete),
-				"deleteUserIDs", resp.Delete,
-				"updateCount", len(resp.Update),
-				"updateUserIDs", updateUserIDs,
-				"updateNicknames", updateNicknames,
-				"insertCount", len(resp.Insert),
-				"insertUserIDs", insertUserIDs)
-			return resp, nil
 		},
 		Full: func(resp *relation.GetIncrementalFriendsResp) bool {
 			return resp.Full
